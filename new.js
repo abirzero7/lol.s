4606049b33f8bd4a1dd887f6eafa0c22d5277626
Md Abir Hossen <abir4605@gmail.com>
2025-02-02 00:25:46 +0600

    Add files via upload

---
+++ new.js
@@ -0,0 +1,8 @@
+let username = "";
+
+while(username === ""|| username === null || username === "no" ||username !== "yes"){
+    username=window.prompt("Are You Gay?");
+
+}
+
+console.log(`i knew it's ${username}`);
